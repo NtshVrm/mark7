939ecee89c04f36a0380e26368644716fede0a65
NtshVrm <nitishvarma2k@gmail.com>
2021-06-14 22:49:41 +0530

    docs: taking text input

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
+var txtInput = document.querySelector("#txt-input");
+
 btnTranslate.addEventListener("click", function clickEventHandler(){
+
     console.log("Clicked!");
+    console.log(txtInput.value);
+
 
 })
