1a0848c86ab2e3f2a2ee8c95331e9db80c64024b
NtshVrm <nitishvarma2k@gmail.com>
2021-06-15 01:06:01 +0530

    mock server call

--- app.js
+++ app.js
@@ -4,9 +4,25 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output-box");
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-    outputDiv.innerText = "TRANSLATED TEXT" + " from " + txtInput.value;
+function getTranslationURL(text) {
 
+    return serverURL + "?" + "text=" + text;
+
+}
+
+
+
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+
+    // outputDiv.innerText = "TRANSLATED TEXT" + " from " + txtInput.value;
+
+    var inputText = txtInput.value; //input text
+
+    //server call for processing
+
+    fetch(getTranslationURL(inputText)).then(response => response.json()).then(json => console.log(json.contents.translated)).catch(errorHandler)
 
 })
