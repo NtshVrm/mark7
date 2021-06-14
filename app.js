1dc0c67b877165d61d7690bad2c60de95e49e9ec
NtshVrm <nitishvarma2k@gmail.com>
2021-06-15 01:18:11 +0530

    docs: output on div box

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ btnTranslate.addEventListener("click", function clickEventHandler() {
 
     //server call for processing
 
-    fetch(getTranslationURL(inputText)).then(response => response.json()).then(json => console.log(json.contents.translated)).catch(errorHandler)
+    fetch(getTranslationURL(inputText)).then(response => response.json()).then(json => {var translatedText = json.contents.translated; outputDiv.innerText = translatedText}).catch(errorHandler)
 
 })
