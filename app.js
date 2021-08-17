a8fee864316bc97561662008fdc264b8bc5c7c99
NtshVrm <nitishvarma2k@gmail.com>
2021-08-17 08:17:48 +0530

    Revert "docs: api change"
    This reverts commit e30f655acce0e294e37c79b277afd85326df0822.

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output-box");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/morse.json"
 
 function getTranslationURL(text) {
 
